4d5b6cc575a7627f22274cbf49443bb36f1832fe
Oleg Kononov <kononov.itwork@gmail.com>
2021-11-29 03:03:35 +0300

    refactor: update package name

--- src/main/java/com/github/kononovit/serviceDataJPA/ServiceApplication.java
+++ src/main/java/com/github/kononovit/serviceDataJPA/ServiceApplication.java
@@ -1,4 +1,4 @@
-package com.github.kononovit.service;
+package com.github.kononovit.serviceDataJPA;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/github/kononovit/serviceDataJPA/ServiceApplicationTests.java
+++ src/test/java/com/github/kononovit/serviceDataJPA/ServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.github.kononovit.service;
+package com.github.kononovit.serviceDataJPA;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
